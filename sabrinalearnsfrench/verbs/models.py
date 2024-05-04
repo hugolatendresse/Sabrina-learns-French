d268629fd977d1b99cd633e02d6a1dc3367c5244
Hugo Latendresse <hugolatendresse@gmail.com>
2024-05-04 11:06:25 -0700

    able to output three random verbs

--- sabrinalearnsfrench/verbs/models.py
+++ sabrinalearnsfrench/verbs/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+class Verb(models.Model):
+    francais = models.CharField(max_length=50)
+    anglais = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.francais + " / " + self.anglais
--- sabrinalearnsfrench/verbs/views.py
+++ sabrinalearnsfrench/verbs/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Verb  # Adjust this import according to your model's actual location
+from django.db.models import F
+
+from .models import Verb
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world!!!")
+    verb_list = Verb.objects.order_by("?")[:3]
+    output =  "Today's verbs are:\n" + " \n ".join([v.francais for v in verb_list])
+    return HttpResponse(output)
+
+    # TODO
+    # # Pass the random verbs to your template
+    # context = {
+    #     'verbs': random_verbs
+    # }
+    # return render(request, 'your_template_name.html', context)
